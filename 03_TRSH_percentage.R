0a67db145ebd1dd9edb50dda0651ef2c3cafbc79
Annegreet <veeken.g.a@gmail.com>
2022-01-04 15:03:46 +0100

    make code tidier

--- 03_TRSH_percentage.R
+++ 03_TRSH_percentage.R
@@ -1,88 +1,50 @@
 # This script filters the pollen data, applies the ppe correction and calculates 
-# pollen percentages of trees and shrubs, binned, for use in the PCA
-# Annegreet Veeken, last changed 5-8-2021
+# pollen percentages of trees and shrubs, for use in the GAMs
+# Annegreet Veeken
 
-# libraries
+# Libraries
 library(tidyverse)
 library(readxl)
-library(xlsx)
 
 ## Prepare pollen data: 
 lPOL <- readRDS("RDS_files/03_PollenWEU-Harmonised.rds")
+sitenames <- lPOL %>% 
+  purrr::map(., ~pull(., site.name)) %>% 
+  unlist() %>% 
+  unique()
+names(lPOL) <- sitenames
 ppe <- read_xlsx("Data/RPP_Dataset_v1_Table_3_4.xlsx")
 ppe <- tibble(ppe.taxon = ppe$`Target taxon`,
               PPE = ppe$`RPP v1 (Northern Hemisphere)`)
-harm <- read_xlsx("Data/HarmonizationTablePollen.xlsx")
-harm <- harm %>% 
-  dplyr::select(AccVarName2, GroupId, ppe.taxon) %>% 
-  distinct() %>% 
-  filter(!is.na(ppe.taxon))
-
-# convert dataframe to long format
-lPOLlong <- lPOL %>% 
-  purrr::map(.,
-             ~dplyr::select_if(., !names(.) %in%
-                                 c(".id", "depth", "age.old", "age.young",
-                                   "date.type", "lat", "long", "dataset"))) %>%
-  purrr::map(., ~gather(., key = "taxa", value = "count", -c(1:4)))
 
 # add ppe taxon and group id 
-lPOLlong <- lPOLlong %>% 
+lPOL <- lPOL %>% 
   purrr::map(. %>% 
-               # WATCH OUT FOR DUPLICATION (check length of each df in list before and after join)
-               left_join(., harm, by = c("taxa" = "AccVarName2")) %>% 
                # filter out non-pollen
-               filter(., !ppe.taxon %in%  "not applicable"))
+               filter(., !ppe.taxon %in%  "not applicable") %>% 
+               # filter out ages past 10000
+               filter(., age <= 10000))
 
-
-# discard samples with low sample count 
-lowcount <- 
-  lPOLlong %>% purrr::map(. %>% 
-                            # filter out no ppe
-                            filter(., !ppe.taxon %in%  "no ppe") %>%
-                            group_by(time.bin) %>% 
-                            summarise(pollencount = sum(count)) %>%
-                            filter(pollencount < 300) %>% 
-                            pull(time.bin)) 
-
-lPOLlong <- 
-  purrr::map2(lPOLlong, 1:length(lowcount), 
-              ~filter(.x, !time.bin %in% lowcount[[.y]])) 
-
-# at what sites is pollen sites too low?
-sitelowcount <- lPOLlong %>% 
-  keep(.,~nrow(.) == 0) %>% 
-  names
-sitelowcount
-
-lPOLlong <- lPOLlong %>% 
-  # filter sites without data (due to low count)
-  discard(., ~nrow(.) == 0) %>% 
-  # filter no ppe data
-  purrr::map(., ~filter(., !ppe.taxon %in%  c("no ppe")) %>% 
-               # select relevant columns
-               dplyr::select(site.name, age, time.bin, 
-                             Time.BP, taxa, GroupId, ppe.taxon,
-                             count))
-
-
-
-# calculate percentage per time bin, ppe corrected
-lPOLcorrected <-  lPOLlong %>%
+# calculate percentage ppe corrected
+lPOLcorrected <-  lPOL %>%
   purrr::map(. %>%
+               filter(!ppe.taxon == "no ppe") %>% 
                # bind ppe data
                left_join(., ppe, by = "ppe.taxon") %>% 
-               # adjusted count with ppe
+               # adjust count with ppe
                mutate(adjustedcount = count*PPE) %>%
-               group_by(time.bin) %>% #
+               group_by(site.name, age, depth) %>% 
+               # calculate percentages
                mutate(adjustedpercent = adjustedcount/sum(adjustedcount),
-                      percent = count/sum(count, na.rm = TRUE),
-                      time.bin = ) %>%
-               group_by(time.bin, GroupId) %>%
+                      percent = count/sum(count, na.rm = TRUE)) %>%
+               group_by(site.name, age, depth, GroupID, .drop = FALSE) %>%
                summarise(adjustedpercent = sum(adjustedpercent, na.rm = TRUE),
-                         percent = sum(percent)) %>%
-               filter(GroupId == "TRSH"))
+                         percent = sum(percent)) %>% 
+               # sort
+               arrange(depth) %>% 
+               filter(GroupID == "TRSH")) 
 
+# check
 lPOLcorrected[[1]]
 
 saveRDS(lPOLcorrected, "RDS_files/03_TRSH_percentage.rds")
